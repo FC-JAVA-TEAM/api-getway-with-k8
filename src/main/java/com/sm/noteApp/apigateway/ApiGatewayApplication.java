9646a6df50d2a66b72602080c4d12eb154c101f1
Rahul-71 <rahultiwari.201307@gmail.com>
2025-03-18 17:55:25 +0530

    updated configs

--- src/main/java/com/sm/noteApp/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/sm/noteApp/apigateway/ApiGatewayApplication.java
@@ -1,29 +1,27 @@
 package com.sm.noteApp.apigateway;
 
+import com.sm.noteApp.apigateway.security.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-
-import com.sm.noteApp.apigateway.security.JwtFilter;
 import org.springframework.web.server.WebFilter;
+
 /*
  * ApiGatewayApplication is the main class containing bean registration for Jwtfilter
- * 
+ *
  * @author Shilpi
  * @since 2025-03-06
  */
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
-	
-	@Bean
+    @Bean
     public WebFilter jwtFilter() {
         return new JwtFilter();
     }
-	 
+
 }
